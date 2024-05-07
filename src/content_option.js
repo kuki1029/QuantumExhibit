2e32bd06d961c993284beb2d858484f1c03fdae5
cluktuke <chat.luktuke2003@gmail.com>
2024-05-07 02:29:22 -0400

    Changed name to Chaitanya, edited about text, final draft

--- src/content_option.js
+++ src/content_option.js
@@ -31,13 +31,14 @@ const kunalData = {
 };
 
 const chatData = {
-    title: "Chat",
-    aboutme: "I am a Honours Physics student at the University of Waterloo and intend to pursue further graduate studies in physics.\
-    Writing about and coding physics simulations has let me explore my interest in physics in multiple ways\
+    title: "Chaitanya",
+    aboutme: "I am a Honours Physics student at the University of Waterloo and an aspiring research scientist.\
+    Developing physics simulations and this site has let me explore my interest in physics in non-traditional ways\
     enabling me to pursue and merge my passion for writing, coding and physics on my own terms. I have experience working in a number of scientific\
     research institutes such as TRIUMF and SNOLAB and have developed experience working with Monte Carlo simulations and Statistical Data Analysis\
-    techniques in ROOT, Python C++ and even Fortran. I have had experience ranging from Dark Matter experiments (Deap-3600 and DarkSide-20K) to\
-    Nuclear Physics and Magneto-Optical Atom Traps (TRINAT)",
+    techniques in ROOT, Python C++ and even Fortran. Working on experiments ranging from Dark Matter experiments (Deap-3600 and DarkSide-20K) to\
+    Nuclear Physics and Magneto-Optical Atom Traps (TRINAT) has allowed me to learn about a wide range of physics sub-fields, a list I hope to\
+    keep growing",
 };
 
 const dataportfolio = [{
