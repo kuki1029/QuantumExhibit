f94745502954f95855f05ffd1cf5b2e13f4c16c4
Purushottam Varkekar <pvarkeka@uwaterloo.ca>
2024-05-19 12:47:14 +0530

    Create new class for animation

--- src/simulationsGraphics/simplePendulum/index.js
+++ src/simulationsGraphics/simplePendulum/index.js
@@ -1,21 +1,16 @@
 import React, { useRef, useEffect, useState } from "react";
 import { Application, Graphics } from 'pixi.js';
 import "./style.css";
-import SimplePendulumData from '../../simulationsCalcs/simplePendulum/calculation.js'
 import { Screen } from "../../constants.js";
-import { MathJax, MathJaxContext } from "better-react-mathjax";
 import Typography from '@mui/material/Typography';
 import Slider from '@mui/material/Slider';
 import Stack from '@mui/material/Stack';
 import { Box } from "@mui/material";
 import { SimePendExplanation } from "./spExplanation.js";
+import simpPendAnimation from "./spAnimation.js";
+import SimplePendulumData from "../../simulationsCalcs/simplePendulum/calculation.js";
 
 
-const pivotSize = 20;
-const defaultLength = 200;
-const defaultMass = 1;
-const pendulumSize = 10;
-const animationSpeed = 100;
 const sliderBoxSize = 180;
 
 // Set intial colors based on users theme
@@ -44,109 +39,36 @@ export const SimplePendulum = () => {
 
   // Handle changes when the slider is changed
   const handleGravSliderChange = (event, newValue) => {
-    pend.setGravity(newValue, totalTime)
+    pend.setGravity(newValue, 0)
     setGravValue(newValue);
   };
 
   // Handle changes when the slider is changed
   const handleLenSliderChange = (event, newValue) => {
-    pend.setLength(newValue, totalTime)
+    pend.setLength(newValue, 0)
     setLengthVal(newValue);
   };
 
-  // React hooks. Runs after render of page
+  // Runs once
   useEffect(() => {
-    // Graphics instance for each object in the pendulum system
-    const pivot = new Graphics();
-    const pendulum = new Graphics();
-    const rope = new Graphics();
-
-    // Draw pivot at half width and a third of the height.
-    // Rect draws at x, y of top left of rectangle so we subtract those
-    // to make sure it properly draws in the center
-    pivot.rect((Screen.width - pivotSize)/ 2, Screen.height/3 - pivotSize/2, pivotSize, pivotSize);
-    pivot.fill(pivotColor);
-
-    // This pendulum location is irrelevant as we update it in the app.ticker
-    // to match the motion of a pendulum
-    pendulum.circle((Screen.width)/ 2, Screen.height/3, pendulumSize)
-    pendulum.fill(pendulumColor)
-
-    drawRope(Screen.width / 2, Screen.height / 3 + defaultLength)
-
-    // Need a function for this as we cannot animate the line moving
-    // by just changing one side. We redraw the line each time
-    // the pendulum moves
-    function drawRope(endpointX, endpointY) {
-      rope.moveTo(Screen.width / 2, Screen.height / 3);
-      rope.lineTo(endpointX, endpointY);
-      rope.stroke({ width: 2, color: ropeColor });
-    }
-
     // We need a function for this as pixiJS requires
     // async setup to be initialized
     async function initializePixiApp() {
       // Create a new application
       const app = new Application();
-      // Initialize the application
-      await app.init({ background: backgroundColor, width: Screen.width, 
-                       height: Screen.height, antialias: true });
-      // Append the application canvas to the document body
+
+      const pendAnimate = new simpPendAnimation(app, Screen.width/2, Screen.height/3)
+      await pendAnimate.initPixi()
+      pendAnimate.drawPivot()
+      pendAnimate.drawPendulum()
+      pendAnimate.drawRope(0, defaultLength)
+  
+      // Attach to the current DOM
       ref.current.appendChild(app.canvas);
-      // Add rect to the application
-      app.stage.addChild(rope)
-      app.stage.addChild(pivot)
-      app.stage.addChild(pendulum)
-      
-      // Runs on each render loop. Used to animate
-      app.ticker.add(() => {
-        totalTime += app.ticker.elapsedMS / animationSpeed;
-        // X, and y are switched as the angle is defind relative to y not x
-        // as it usually is for polar to cartesian conversions
-        const xCoord = pend.getCurrentPosition(totalTime)[0]
-        const yCoord = pend.getCurrentPosition(totalTime)[1]
-        pendulum.x = yCoord
-        pendulum.y = xCoord
-        // Animate rope
-        rope.clear();
-        drawRope(yCoord + (Screen.width / 2), xCoord + defaultLength)
-
-        // Redraw with new color to match theme
-        if (redrawColors) {
-          pendulum.fill(pendulumColor);
-          pendulum.circle((Screen.width)/ 2, Screen.height/3, pendulumSize)
-
-          pivot.rect((Screen.width - pivotSize)/ 2, Screen.height/3 - pivotSize/2, pivotSize, pivotSize);
-          pivot.fill(pivotColor);
-
-          app.renderer.background.color = backgroundColor;
-          
-          redrawColors = false;
-        }
-      });
     }
 
     initializePixiApp();
   }, []);
-
-  // Colors for simulation. If not light, assume dark
-  // We emit an event in the themeToggle component and listen to it here
-  // Whenever theme changes, we update our colors and redraw shapes
-  window.addEventListener('themeChanged', () => {
-    const theme = localStorage.getItem("theme");
-    redrawColors = true;
-    if (theme === 'light') {
-      pivotColor = "#000000";
-      pendulumColor = 0x4169E1;
-      backgroundColor = '#ffffff'; 
-      ropeColor = pivotColor;
-    } else {
-      pivotColor = "#ffffff";
-      pendulumColor = 0xff0033;
-      backgroundColor = 0x0c0c0c;
-      ropeColor = pivotColor;
-    }
-  })
 
   // Return the actual code for the Pixi.JS. 
   return (
---
+++ src/simulationsGraphics/simplePendulum/spAnimation.js
@@ -0,0 +1,136 @@
+import { Screen } from "../../constants.js";
+import { Graphics } from 'pixi.js';
+import SimplePendulumData from '../../simulationsCalcs/simplePendulum/calculation.js'
+
+// Constants for animation
+const pivotSize = 20;
+const defaultLength = 200;
+const defaultMass = 1;
+const pendulumSize = 10;
+const animationSpeed = 100;
+
+const pend = new SimplePendulumData(defaultMass, defaultLength);
+
+/** Class creates the application for Pixi.JS and helper
+ * functions to do all the drawing for it.
+*/
+export default class simpPendAnimation { 
+    /**
+     * Initializes the application for Pixi.JS
+     * @param {Renderer} app - Pixi.JS renderer
+     * @param {number} originX - The origin for x values. All objects will be drawn from here
+     * @param {number} originY - The origin for y values. All objects will be drawn from here
+     */
+    constructor(app, originX, originY) {
+        this.app = app;
+        this.originX = originX
+        this.originY = originY
+        // totalTime used to update the animation
+        this.totalTime = 0;
+
+        // Initialize all graphics instances
+        this.pendulum = new Graphics();
+        this.rope = new Graphics();
+        this.pivot = new Graphics();
+        this.app.stage.addChild(this.pivot)
+        this.app.stage.addChild(this.rope)
+        this.app.stage.addChild(this.pendulum)
+
+        // Set intial colors based on users theme
+        const theme = localStorage.getItem("theme")
+        this.pivotColor = (theme === 'light') ? "#000000" : "#ffffff";
+        this.pendulumColor = (theme === 'light') ? 0x4169E1 : 0xff0033;
+        this.backgroundColor = (theme === 'light') ? '#ffffff' : '#0c0c0c';
+        this.ropeColor = this.pivotColor;
+    }
+
+    /**
+     * Draws pivot. Draws it at the origin point
+     */
+    drawPivot() {
+        // Need to calculate the coordinates of the upper left of rectangle as Pixi draws it from upper
+        // left corner
+        const upperLeftX = this.originX - pivotSize/ 2
+        const upperLeftY = this.originY - pivotSize/2
+        this.pivot.rect(upperLeftX, upperLeftY, pivotSize, pivotSize);
+        this.pivot.fill(this.pivotColor);
+    }
+
+    /**
+     * Draws rope. Draws from origin to labelled x,y. All coordinates 
+     * are relative to the origin which was passed in when initializing this class
+     * @param {number} x - The second point to which we draw the line. First point is at origin
+     * @param {number} y - The second point to which we draw the line. First point is at origin
+     */
+    drawRope(x, y) {
+        this.rope.moveTo(this.originX, this.originY);
+        this.rope.lineTo(this.originX + x, y); 
+        this.rope.stroke({ width: 2, color: this.ropeColor });
+    }
+
+     /**
+     * Draws pendulum. All coordinates 
+     * are relative to the origin which was passed in when initializing this class
+     */
+    drawPendulum() {
+        this.pendulum.circle(this.originX, this.originY, pendulumSize)
+        this.pendulum.fill(this.pendulumColor)
+    }
+
+    /**
+     * Adds a ticker to the app instance of Pixi.Js. Runs every frame to 
+     * help with animation. All animation is handled in here
+     */
+    animatePendulumRope() {
+        // Runs on each render loop. Used to animate
+        this.app.ticker.add(() => {
+            this.totalTime += this.app.ticker.elapsedMS / animationSpeed;
+            // X, and y are switched as the angle is defind relative to y not x
+            // as it usually is for polar to cartesian conversions
+            const xCoord = pend.getCurrentPosition(this.totalTime)[0]
+            const yCoord = pend.getCurrentPosition(this.totalTime)[1]
+            // Animate the pendulum
+            this.pendulum.x = yCoord
+            this.pendulum.y = xCoord
+            // Animate rope
+            this.rope.clear();
+            this.drawRope(yCoord, xCoord + defaultLength)
+        });
+    }   
+
+    /**
+     * Colors for simulation. If not light, assume dark
+     * We emit an event in the themeToggle component and listen to it here
+     * Whenever theme changes, we update our colors and redraw shapes
+     */
+    updateColors() {
+        // Colors for simulation. If not light, assume dark
+        // We emit an event in the themeToggle component and listen to it here
+        // Whenever theme changes, we update our colors and redraw shapes
+        window.addEventListener('themeChanged', () => {
+            if (localStorage.getItem("theme") === 'light') {
+                this.pivotColor = "#000000";
+                this.pendulumColor = 0x4169E1;
+                this.backgroundColor = '#ffffff'; 
+                this.ropeColor = this.pivotColor;
+            } else {
+                this.pivotColor = "#ffffff";
+                this.pendulumColor = 0xff0033;
+                this.backgroundColor = 0x0c0c0c;
+                this.ropeColor = this.pivotColor;
+            }
+            this.drawPendulum()
+            this.drawPivot()
+            this.app.renderer.background.color = this.backgroundColor;
+        })
+    }
+
+    async initPixi() {
+        await this.app.init({ background: this.backgroundColor, width: Screen.width, 
+            height: Screen.height, antialias: true });
+        // Set colors to be reactive to theme changes
+        this.updateColors();
+        // Run animation
+        this.animatePendulumRope()
+    }
+}
