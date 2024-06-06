8b592e7934bd88e2b99a930b423f7f1b83b7ff4b
cluktuke <chat.luktuke2003@gmail.com>
2024-06-06 01:19:18 -0600

    Created scalar and vector ode step solutions, and updated the solve and change h class functions to complete euler method overhaul

--- src/odeSolvers/euler.js
+++ src/odeSolvers/euler.js
@@ -2,6 +2,11 @@
  * Basic Class Implementation of Eulers Forward propagation method
  */
 
+import {addArrays, 
+    multiplyArrayByScalar, 
+    applyParamsToAllFunc } from "./odeUtility";
+
+
 export class eulerforward{
     /** Preliminary euler method solver that takes in a function and an initial t value 
     * @param {function(t, y) | function(t, [y])[]} func - Functional form of the derivative to be propagated
@@ -59,21 +64,29 @@ export class eulerforward{
      */
     
     solve(func = this.func, y0 = this.y0, h = this.h0, t0=this.t0, tlim){
-        let ynought = y0
-        let t = tlim
-        let y_tph = 0
-        let y_num = []
-        let t_vals = []
-        while (t<=tlim){
-            y_tph = ynought + h*func(t)
-            y_num = y_num.push(y_tph)
-            t_vals = t_vals.push(t)
-
-            ynought = y_tph
-            t = t + h
+        let y = y0
+        let t = t0
+        
+        if (t0>tlim){
+            throw new RangeError("The final time value should be greater than the initial time value")
+        }
+        let solved = []
+        if (this.isArray) {
+            while (t<tlim+h){
+                solved.push([t, y])
+                y = addArrays(y, this.odeMethodVector(t, y, h))
+                t +=h
+            }   
+        }
+        else{
+            while (t<tlim+h){
+                solved.push(t, y)
+                y = y + this.odeMethodScalar(t, y, h))
+                t += h
+            }
         }
         
-        return [y_num, t_vals]
+        return solved
     }
     /**To DO: Implement a first and second derivative calculator, customise for pendulum? */
 
@@ -86,12 +99,31 @@ export class eulerforward{
      * @returns {number} - Returns the term to be added to y0 for the next y value
      */
     odeMethodScalar(t, y, h) {
-        const y_del = y + h*this.func(t, y)
+        const y_del = h*this.func(t, y)
         
         return y_del
     }
 
-    /* firstder(func = this.func, ){ */
+    /**
+     * Returns the constants or terms required for the ode scheme for scalar variables. Euler Method
+     * and returns the addition term for it.
+     * @param {number} t - The current t value or t_n
+     * @param {number[]} y - The current y value or y_n
+     * @param {number} h - Step size
+     * @returns {number []} - Returns the term to be added to y0 for the next y value
+     */
+    odeMethodVector(t, y, h) {
+        const y_del = multiplyArrayByScalar(h, applyParamsToAllFunc(this.func, t, y))
+        
+        return y_del
+    }
+
+    /**  Changes the step size to new value
+     * @param newH New value for the step size
+     */
+    changeStepSize(newH) {
+        this.h = newH
+    }
 
     
 
