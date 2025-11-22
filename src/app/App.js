559d7b7890d0a1704ab4dad8aa352342ee9c28f4
Purushottam Varkekar <pvarkeka@uwaterloo.ca>
2025-11-22 18:47:16 -0500

    Migrate from Create React App to Vite

--- src/app/App.js
+++ src/app/App.js
@@ -1,19 +1,23 @@
-import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {
   BrowserRouter as Router,
 } from "react-router-dom";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import AppRoutes from "./routes";
 import Headermain from "../header";
 import Footer from "../components/footer";
 import "./App.css";
 
+const theme = createTheme();
+
 export default function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-        <Headermain />
-        <AppRoutes />
-      <Footer />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router basename={import.meta.env.BASE_URL}>
+          <Headermain />
+          <AppRoutes />
+        <Footer />
+      </Router>
+    </ThemeProvider>
   );
 }
---
+++ vite.config.js
@@ -0,0 +1,48 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      jsxRuntime: 'automatic',
+    }),
+  ],
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.js$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    include: [
+      '@mui/material',
+      '@mui/icons-material',
+      '@emotion/react',
+      '@emotion/styled',
+    ],
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+  ssr: {
+    noExternal: ['@mui/material', '@mui/icons-material'],
+  },
+  assetsInclude: ['**/*.JPG', '**/*.JPEG', '**/*.PNG', '**/*.GIF', '**/*.SVG'],
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'build',
+    sourcemap: false,
+    commonjsOptions: {
+      include: [/node_modules/],
+      transformMixedEsModules: true,
+    },
+  },
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+  },
+})
